e9a8bdd33c88db7eacce7ae807c16e4d314048d7
DuRuixue <Anchorer@DuRuixuedeMacBook-Pro.local>
2019-02-18 16:31:20 +0800

    feat: first commit

---
+++ prac.py
@@ -0,0 +1,12 @@
+x = 5
+print (x)
+
+list_x = [3, "hello", 1, 7]
+print (list_x)
+print ("list_x[0]=" + list_x[0])
+print ("list_x[-1]=" + list_x[-1])  # 访问最后的元素
+print ("list_x[-2]=" + list_x[-2])  # 访问倒数第二个元素
+
+
+
+
